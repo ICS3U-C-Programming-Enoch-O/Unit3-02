de4a41004b1ebe3ea7013ab05b42690daac59c95
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-03-21 16:35:19 +0000

    num_guess

---
+++ “num_guess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Enoch O
+# Created on: Feb 27, 2025
+# This program calculates the circumference and area of a circle.
+
+SECRET_NUMBER = 5  # Choose a secret number
+
+# Get user's guess
+user_guess = int(input("Enter a number between 0-9: "))
